d14b1d89524e1c0dbde19dd885d072b41198c49c
Seth <slionel1850@gmail.com>
2023-06-19 20:10:49 +0000

    Allow X-Frame

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                key: 'X-Frame-Options',
+                value: 'ALLOW-FROM https://twitch.tv'
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
