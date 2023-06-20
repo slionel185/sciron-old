050d0c8560c89e1f55fc060b8cb9b1e7625971ee
Seth <slionel1850@gmail.com>
2023-06-20 21:51:20 +0000

    ReworkPricing

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -13,18 +13,18 @@ export default function pricing() {
     const [showYearly, setShowYearly] = useState(false)
 
     return (
-        <div className='flex flex-col justify-start items-center pt-16 pb-4 lg:h-screen w-full gap-4'>
+        <div className='flex flex-col justify-center items-center pt-16 pb-4 lg:h-screen w-full gap-4'>
             <div className='flex flex-col gap-4 justify-center items-center max-w-lg xl:max-w-2xl'>
                 <Image src={Logo} alt='Logo' height={128} width={128} className='-mt-12 -m-6' />
                 <h1 className='text-5xl italic font-bold uppercase text-primary shadow-xl'>sciron</h1>
                 <p className='text-xl font-medium text-center px-4'>Here at Sciron we pride ourselves in delivering a user platform that connects your communities. From Twitch to Discord we help you manage everything while keeping your stream at the forefront!</p>
             </div>
-            <div className='flex h-full flex-col gap-8 lg:flex-row w-full justify-center items-center px-8 pb-4'>
-                <div id='free' className='card w-full md:w-96 lg:h-full bg-base-300 shadow-xl'>
+            <div className='flex flex-col gap-8 lg:flex-row w-full justify-center items-center px-8 pb-4 pt-16'>
+                <div id='free' className='card w-full md:w-96 max-h-[36rem] lg:h-full bg-base-300 shadow-xl'>
                     <div className='card-body'>
                         <h2 className='w-full text-2xl text-center italic uppercase font-bold'>Starter</h2>
                         <p className='w-full text-center text-xl'>Great for new audiences to have a space to grow</p>
-                        <h1 className='text-4xl py-2 pb-3 italic text-center'>$0<span className='text-sm'>/mo</span></h1>
+                        <h1 className='text-4xl h-full justify-center py-2 pb-3 italic text-center'>$0<span className='text-sm'>/mo</span></h1>
                         <ul className='px-4 flex flex-col gap-2'>
                             <li className='flex justify-start items-center gap-4'>
                                 <BsCheck2Circle className='text-xl text-success' />
@@ -60,7 +60,7 @@ export default function pricing() {
                         </div>
                     </div>
                 </div>
-                <div id='pro' className='card w-full md:w-96 lg:h-full bg-base-300 shadow-xl'>
+                <div id='pro' className='card w-full md:w-96 max-h-[36rem] lg:h-full bg-base-300 shadow-xl'>
                     <div className='card-body'>
                         <h2 className='w-full text-2xl text-center italic uppercase font-bold'>Hobbyist</h2>
                         <p className='w-full text-center text-xl'>Streamers that want to add some customization to their stream chat!</p>
@@ -100,7 +100,7 @@ export default function pricing() {
                         </div>
                     </div>
                 </div>
-                <div id='Lifetime' className='card w-full md:w-96 lg:h-full bg-base-300 shadow-xl'>
+                <div id='Lifetime' className='card w-full md:w-96 max-h-[36rem] lg:h-full bg-base-300 shadow-xl'>
                     <div className='card-body'>
                         <h2 className='w-full text-2xl text-center italic uppercase font-bold'>Professional</h2>
                         <p className='w-full text-center text-xl'>Made for the utilitarian who needs all the commands they can get!</p>
