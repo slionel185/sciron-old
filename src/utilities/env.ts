ff1b25d7d9bdfaa80c52577974178352bdaa690f
Seth <slionel1850@gmail.com>
2023-06-20 03:30:44 +0000

    attemptToFix?

--- src/utilities/env.ts
+++ src/utilities/env.ts
@@ -2,9 +2,6 @@ import { z } from 'zod'
 import { createEnv } from '@t3-oss/env-nextjs'
 
 export const env = createEnv({
-    client: {
-        NEXT_PUBLIC_PARENT_URL: z.string()
-    },
     server: {
         DATABASE_URL: z.string(),
         NEXTAUTH_URL: z.string().url(),
@@ -12,8 +9,11 @@ export const env = createEnv({
         TWITCH_CLIENT_ID: z.string(),
         TWITCH_CLIENT_SECRET: z.string(),
     },
+    client: {
+        NEXT_PUBLIC_PARENT_URL: z.string()
+    },
     runtimeEnv: {
-        NEXT_PUBLIC_PARENT_URL: process.env.NEXTAUTH_URL,
+        NEXT_PUBLIC_PARENT_URL: process.env.NEXT_PUBLIC_PARENT_URL,
         DATABASE_URL: process.env.DATABASE_URL,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
